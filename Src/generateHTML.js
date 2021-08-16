6f6799c2b97839142719dc12a981914380e92989
Blake Schumann <blakeschumann15@gmail.com>
2021-08-16 18:57:14 +0930

    creating intern card

--- Src/generateHTML.js
+++ Src/generateHTML.js
@@ -9,7 +9,7 @@ const generateManager = function(manager) {
     <div class="card-body">
         <p class="id">ID: ${manager.id}</p>
         <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
-        <p class="office">Office Number ${manager.officeNumber}</p>
+        <p class="office">Office Number: ${manager.officeNumber}</p>
     </div>
     </div>
     </div>
@@ -36,18 +36,18 @@ const generateEngineer = function(engineer) {
 }
  
 
-const generateManager = function(manager) {
+const generateIntern = function(intern) {
     return `
     <div class="col-4 mt-4">
     <div class="card h-100"></div>
     <div class="card-header">
-        <h3>${manager.name}</h3>
-        <h4>Manager</h4><i class="material-icons">content_paste</i>
+        <h3>${intern.name}</h3>
+        <h4>Intern</h4><i class="material-icons">assignment_ind</i>
     </div>
     <div class="card-body">
-        <p class="id">ID: ${manager.id}</p>
-        <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
-        <p class="office">Office Number ${manager.officeNumber}</p>
+        <p class="id">ID: ${intern.id}</p>
+        <p class="email">Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+        <p class="School">School: ${intern.school}</p>
     </div>
     </div>
     </div>
