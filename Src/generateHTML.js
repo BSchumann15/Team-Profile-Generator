5e7e836856d6c6010a95e106371d8e6c8e11db10
Blake Schumann <blakeschumann15@gmail.com>
2021-08-16 19:05:58 +0930

    generating array  for employee cards

--- Src/generateHTML.js
+++ Src/generateHTML.js
@@ -52,4 +52,31 @@ const generateIntern = function(intern) {
     </div>
     </div>
     `;
+};
+
+
+generateHTML = (data) => {
+    pageArray = [];
+    for (let i = 0; i > data.length; i++) {
+        const employee = data[i];
+        const role = employee.getRole();
+
+
+        if (role === 'Manager') {
+            const managerCard = generateManager(employee);
+            pageArray.push(managerCard)
+        }
+
+        if (role === 'Engineer') {
+            const engineerCard = generateEngineer(employee);
+            pageArray.push(engineerCard);
+        }
+
+        if (role === 'Intern') {
+            const internCard = generateIntern(employee);
+            pageArray.push(internCard);
+        }
+    }
+
+    
 }
